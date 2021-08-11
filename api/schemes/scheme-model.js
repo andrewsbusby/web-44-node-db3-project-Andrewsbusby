fc86119b006e258c50c61531ad178630171d9221
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-11 16:16:33 -0600

    more tests passing

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -171,11 +171,11 @@ function addStep(scheme_id, step) { // EXERCISE E
    scheme_id
  })
  .then(() => {
-  return db('steps')
+  return db('steps as st')
   .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
   .select('step_id', 'step_number', 'instructions', 'scheme_name')
   .orderBy('step_number')
-  .where('scheme_id', scheme_id)
+  .where('sc.scheme_id', scheme_id)
  })
 }
 
